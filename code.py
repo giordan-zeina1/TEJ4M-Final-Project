f3f7657f3e517d756cdbd39695dff5b857b7cf07
giordan-zeina1 <giordan.zeina@stu.ocsb.ca>
2026-01-21 19:29:46 +0000

    Code doesn't work

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 """
 Created by: Giordan Zeina
@@ -12,6 +12,7 @@ import ugame
 import random
 import time
 
+
 def splash_scene():
     image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
     background = stage.Grid(image_bank_background, 10, 8)
@@ -19,24 +20,23 @@ def splash_scene():
     game.layers = [background]
     game.render_block()
 
-    try:
-        coin_sound = open("coin.wav", "rb")
-        ugame.audio.play(coin_sound)
-    except: pass # Skip if file is missing
+    coin_sound = open("coin.wav", "rb")
+    ugame.audio.play(coin_sound)
 
     time.sleep(2.0)
     menu_scene()
 
+
 def menu_scene():
     image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
     background = stage.Grid(image_bank_mt_background, 10, 8)
-    
+
     text = []
-    text1 = stage.Text(width=29, height=12, palette=constants.RED_PALETTE)
-    text1.move(20, 40)
-    text1.text("PENALTY SHOOTOUT")
-    text.append(text1)
-    
+    title_text = stage.Text(width=29, height=12, palette=constants.RED_PALETTE)
+    title_text.move(20, 40)
+    title_text.text("PENALTY SHOOTOUT")
+    text.append(title_text)
+
     game = stage.Stage(ugame.display, constants.FPS)
     game.layers = text + [background]
     game.render_block()
@@ -47,17 +47,18 @@ def menu_scene():
             game_scene()
         game.tick()
 
+
 def game_scene():
     score = 0
     rounds = 0
-    
+
     score_text = stage.Text(width=29, height=14, palette=constants.RED_PALETTE)
     score_text.move(1, 1)
-    score_text.text("Score: 0")
+    score_text.text("Score: 0 / 0")
 
     image_bank_sprites = stage.Bank.from_bmp16("football_sprites.bmp")
     image_bank_bg = stage.Bank.from_bmp16("football_pitch.bmp")
-    
+
     background = stage.Grid(image_bank_bg, 10, 8)
     keeper = stage.Sprite(image_bank_sprites, 0, 72, 40)
     ball = stage.Sprite(image_bank_sprites, 1, 72, 100)
@@ -70,31 +71,36 @@ def game_scene():
         keys = ugame.buttons.get_pressed()
         player_choice = None
 
-        if keys & ugame.K_LEFT and keys & ugame.K_UP: player_choice = "TL"
-        elif keys & ugame.K_RIGHT and keys & ugame.K_UP: player_choice = "TR"
-        elif keys & ugame.K_UP: player_choice = "M"
-        elif keys & ugame.K_LEFT: player_choice = "BL"
-        elif keys & ugame.K_RIGHT: player_choice = "BR"
+        if keys & ugame.K_LEFT and keys & ugame.K_UP:
+            player_choice = "TL"
+        elif keys & ugame.K_RIGHT and keys & ugame.K_UP:
+            player_choice = "TR"
+        elif keys & ugame.K_UP:
+            player_choice = "M"
+        elif keys & ugame.K_LEFT:
+            player_choice = "BL"
+        elif keys & ugame.K_RIGHT:
+            player_choice = "BR"
 
         if player_choice and rounds < 5:
             rounds += 1
             computer_choice = random.choice(list(constants.LOCATIONS.keys()))
 
-            b_pos = constants.LOCATIONS[player_choice]
-            k_pos = constants.LOCATIONS[computer_choice]
-            
-            ball.move(b_pos[0], b_pos[1])
-            keeper.move(k_pos[0], k_pos[1])
+            ball_pos = constants.LOCATIONS[player_choice]
+            keeper_pos = constants.LOCATIONS[computer_choice]
+
+            ball.move(ball_pos[0], ball_pos[1])
+            keeper.move(keeper_pos[0], keeper_pos[1])
 
             if player_choice != computer_choice:
                 score += 1
-            
+
             score_text.clear()
             score_text.text("Score: {0} / {1}".format(score, rounds))
             game.render_block()
             time.sleep(1.0)
 
-            # Reset
+            # Reset positions
             ball.move(72, 100)
             keeper.move(72, 40)
             game.render_block()
@@ -102,7 +108,9 @@ def game_scene():
         if rounds >= 5:
             time.sleep(2.0)
             menu_scene()
+
         game.tick()
+
 
 if __name__ == "__main__":
     splash_scene()
--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 """
 Created by: Giordan Zeina
@@ -13,11 +13,11 @@ SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 FPS = 60
 
-# Palette for Text (16 colors)
+# Palette for Text
 RED_PALETTE = (0xffe0, 0x1a76, 0x0000, 0xff00, 0xffaf, 0xff51, 0xffa8, 0xff00,
                0x9068, 0x8633, 0x3010, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000)
 
-# Shot selection (x, y) coordinates for sprites
+# Shot selection for sprites
 LOCATIONS = {
     'TL': (40, 40), 'M': (72, 40), 'TR': (104, 40),
     'BL': (40, 80), 'BR': (104, 80)
